a76d72c976c4094b2c983938dc4844f14dbbdee5
Julinhio <julinhio@hotmail.com>
2026-05-14 14:17:48 +0700

    chore: switch Gemini to manual trigger, add test file

---
+++ test.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add(2, 3))
+console.log(divide(10, 0))
+
+// ceci est un test pour toi Gemini!! Dis-moi si tu vois mon message :)
